20de4096af124f75b705c64626e9afbb5987d189
liujiyong <liujiyong@feinno.com>
2017-12-08 18:24:11 +0800

    spring_transaction

--- spring_transaction/trans_web/src/cn/tang/spring/demo1/AccountServiceImpl.java
+++ spring_transaction/trans_web/src/cn/tang/spring/demo1/AccountServiceImpl.java
@@ -1,5 +1,9 @@
 package cn.tang.spring.demo1;
 
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
 //转账案例的业务层实现类
 public class AccountServiceImpl implements AccountService {
     //注入转账的DAO的类
@@ -9,14 +13,27 @@ public class AccountServiceImpl implements AccountService {
         this.accountDao = accountDao;
     }
 
+    //进入事物管理的模板
+    private TransactionTemplate transactionTemplate;
+
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.transactionTemplate = transactionTemplate;
+    }
+
     /**
      *
      * @param out   转出账号
      * @param in    转入账号
      * @param money 转账金额
      */
-    public void transfer(String out, String in, Double money) {
-        accountDao.outMoney(out, money);
-        accountDao.inMoney(in, money);
+    public void transfer(final String out, final String in, final Double money) {
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                accountDao.outMoney(out, money);
+//                int i = 1 / 0;
+                accountDao.inMoney(in, money);
+            }
+        });
     }
 }
